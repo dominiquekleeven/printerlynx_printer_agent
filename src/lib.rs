33e34646312cf68ed55fd62a8cfc6401df7edd77
Dominique <10584854+dominiquekleeven@users.noreply.github.com>
2023-10-22 14:13:38 +0200

    dotenv for tracing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
+use dotenvy::dotenv;
+use tracing::info;
+
 pub mod common;
 
 pub async fn start() {
+    dotenv().expect(".env file not found");
+    tracing_subscriber::fmt().compact().with_target(true).init();
+
+    info!("Starting up...");
+
     todo!("Register the agent with the server, then start the agent.")
 }
