2aca2c1c026ea26fcc85c47b64b2009e2f651794
Dominique <10584854+dominiquekleeven@users.noreply.github.com>
2023-10-27 11:56:54 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use dotenvy::dotenv;
+use tokio_serial::available_ports;
 use tracing::info;
 
 pub mod common;
@@ -8,6 +9,22 @@ pub async fn start() {
     tracing_subscriber::fmt().compact().with_target(true).init();
 
     info!("Starting up...");
+    check_serial_connections();
 
     todo!("Register the agent with the server, then start the agent.")
+}
+
+pub fn check_serial_connections() -> bool {
+    let ports = available_ports().expect("No ports found!");
+
+    // It should also be noted that on macOS, both the Callout (/dev/cu.*) and Dial-in ports (/dev/tty.*)
+    // ports are enumerated, resulting in two available ports per connected serial device.
+    info!("Detected {} serial ports", ports.len());
+
+    // list serial ports
+    for p in ports {
+        info!("Serial port: {:?}", p)
+    }
+
+    true
 }
