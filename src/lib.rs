52030aac4abc5a0b331df41403b21dacd6227d58
Dominique <10584854+dominiquekleeven@users.noreply.github.com>
2023-10-30 20:47:42 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,28 @@ use tracing::info;
 pub mod common;
 pub mod infra;
 
+// const BAUD_RATE: u32 = 115_200;
+
 pub async fn start() {
     dotenv().expect(".env file not found");
     tracing_subscriber::fmt().compact().with_target(true).init();
-
     info!("Starting up...");
     check_serial_connections();
 }
 
 /// Checks and logs the connected serial devices/ports.
-/// - It should also be noted that on macOS, both the Callout (/dev/cu.*) and Dial-in ports (/dev/tty.*)
-/// ports are enumerated, resulting in two available ports per connected serial device.
 pub fn check_serial_connections() {
     let ports = available_ports().expect("No ports found!");
 
-    info!("Detected {} serial ports", ports.len());
+    // on macOS, both the Callout (/dev/cu.*) and Dial-in ports (/dev/tty.*) are double-listed
     for port in ports {
-
         match port.port_type {
             SerialPortType::UsbPort(usb_port_info) => {
+
+                // ignore callout ports, we're using the dial-in ports (similar to Arduino IDE)
+                if port.port_name.contains("cu.") {
+                    continue;
+                }
                 info!("Port: {}", port.port_name);
                 info!("USB device: {:?}", usb_port_info);
             },
