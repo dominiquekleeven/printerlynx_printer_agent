321b513240b8022075452ac4464cd75ccae88baa
Dominique <10584854+dominiquekleeven@users.noreply.github.com>
2023-11-02 12:12:17 +0100

    init adapter fn, dyn adapter.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use crate::adapters::agent_adapter::AgentAdapter;
 use crate::adapters::serial::serial_agent_adapter::SerialAgentAdapter;
+use crate::common::app_error::AppError;
 use dotenvy::dotenv;
 use tracing::{info, warn};
 
@@ -15,11 +16,20 @@ pub async fn start() {
 
     // currently hardcoded to use the serial adapter, use env vars to switch between adapters
     let adapter = SerialAgentAdapter::default();
+    init_adapter(Box::new(adapter)).await.expect("Failed to initialize adapter");
+}
+
+
+pub async fn init_adapter(adapter: Box<dyn AgentAdapter>) -> Result<(), AppError> {
     match adapter.setup() {
         Ok(_) => {
             info!("Adapter setup finished");
             adapter.start().expect("Failed to start adapter");
+            Ok(())
         }
-        Err(e) => warn!("Failed to setup adapter: {}", e),
+        Err(e) => {
+            warn!("Failed to setup adapter: {}", e);
+            Err(e)
+        }
     }
 }
